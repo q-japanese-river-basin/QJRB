aada6a8076349e6569b71e46ddde54a6798ee8eb
Nobusuke IWASAKI <wata909@gmail.com>
2021-09-14 12:27:00 +0900

    version up

--- program/qgis_lrs.bat
+++ program/qgis_lrs.bat
@@ -6,10 +6,13 @@ set GDAL_DATA=%OSGEO4W_ROOT%share\gdal
 set GEOTIFF_CSV=%OSGEO4W_ROOT%share\epsg_csv
 set PROJ_LIB=%OSGEO4W_ROOT%share\proj
 set QGIS_PREFIX_PATH=%OSGEO4W_ROOT%qgis
+rem set QGIS_PREFIX_PATH=D:/OSGeo4W/apps/qgis
 set QT_RASTER_CLIP_LIMIT=4096
 set GDAL_FILENAME_IS_UTF8=YES
 set VSI_CACHE=TRUE
 set VSI_CACHE_SIZE=1000000
-set QT_PLUGIN_PATH=%OSGEO4W_ROOT%qgis\bin\qtplugins;%OSGEO4W_ROOT%\libs\qtplugins
+set QT_PLUGIN_PATH=%OSGEO4W_ROOT%qgis\qtplugins;%OSGEO4W_ROOT%libs\qtplugins
 
 start "QGIS_LRS" /B qgis_lrs.exe
+rem start "cmd" /B cmd.exe
+
